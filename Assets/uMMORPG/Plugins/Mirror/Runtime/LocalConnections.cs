b69a7ea16fdc42a43f83d101371609f3b3979d7b
Micheal Earl <michealearldev@gmail.com>
2019-02-24 16:33:21 -0800

    Init

---
+++ Assets/uMMORPG/Plugins/Mirror/Runtime/LocalConnections.cs
@@ -0,0 +1,42 @@
+using System;
+using UnityEngine;
+
+namespace Mirror
+{
+    // a server's connection TO a LocalClient.
+    // sending messages on this connection causes the client's handler function to be invoked directly
+    class ULocalConnectionToClient : NetworkConnection
+    {
+        public LocalClient localClient { get; }
+
+        public ULocalConnectionToClient(LocalClient localClient) : base ("localClient")
+        {
+            this.localClient = localClient;
+        }
+
+        protected override bool SendBytes(byte[] bytes, int channelId = Channels.DefaultReliable)
+        {
+            localClient.InvokeBytesOnClient(bytes);
+            return true;
+        }
+    }
+
+    // a localClient's connection TO a server.
+    // send messages on this connection causes the server's handler function to be invoked directly.
+    internal class ULocalConnectionToServer : NetworkConnection
+    {
+        public ULocalConnectionToServer() : base("localServer")
+        {
+        }
+
+        protected override bool SendBytes(byte[] bytes, int channelId = Channels.DefaultReliable)
+        {
+            if (bytes.Length == 0)
+            {
+                Debug.LogError("LocalConnection:SendBytes cannot send zero bytes");
+                return false;
+            }
+            return NetworkServer.InvokeBytes(this, bytes);
+        }
+    }
+}
